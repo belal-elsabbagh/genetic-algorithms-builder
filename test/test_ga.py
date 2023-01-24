d3705f15b5a7a15af227f67f6b19cad3aa088b18
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2023-01-24 13:45:43 +0200

    shortest path doesnt actually get the shortest path

--- test/test_ga.py
+++ test/test_ga.py
@@ -29,7 +29,7 @@ def test_target_string():
 
 def path_cost(ind, target):
     def get_cost_of_edge(start, end):
-        return EUROPE_GRAPH.keys().get(start).get(end, 10000)
+        return EUROPE_GRAPH.get(start).get(end, 10000)
 
     def get_cost_of_path(path):
         return sum([get_cost_of_edge(previous, current) for previous, current in zip(path, path[1:])])
@@ -40,36 +40,15 @@ def path_cost(ind, target):
     return get_cost_of_path(ind.get_chromosome())
 
 
-def crossover(ind1, ind2):
-    prob = random.random()
-    child_chromosome = []
-    if prob < 0.45:
-        child_chromosome = ind1.get_chromosome()
-        child_chromosome.append(ind2.get_chromosome()[-1])
-    elif prob < 0.90:
-        child_chromosome = ind2.get_chromosome()
-        child_chromosome.append(ind1.get_chromosome()[-1])
-    else:
-        child_chromosome.append(None)
-    return Individual(child_chromosome)
-
-
-def mutate(ind):
-    new_ind = ind.add_mutations(list(EUROPE_GRAPH.keys()))
-    new_ind.get_chromosome().append(random.choice(list(EUROPE_GRAPH.keys())))
-    return new_ind
-
-
 def test_shortest_path():
     """Test the genetic algorithm with a given target function."""
-    target = ('Arad', 'Bucharest')
+    target = ['Arad', 'Sibiu', 'RimnicuVilcea', 'Pitesti', 'Bucharest']
     ga = GeneticAlgorithmFactory().create(
-        fitness=path_cost,
+        fitness=match_target,
         crossover=mate,
-        mutate=mutate,
+        mutate=lambda x: x.add_mutations(list(EUROPE_GRAPH.keys())),
     )
     population = Individual.random_population(
-        100, 1, ['Arad'] + list(EUROPE_GRAPH.keys()))
+        100, len(target), list(EUROPE_GRAPH.keys()))
     res = ga.run(population, 2000, target, log=True)
-    assert res[0].get_chromosome()[0] == target[0]
-    assert res[0].get_chromosome()[-1] == target[1]
+    assert res[0].get_chromosome() == target
